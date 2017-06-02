d3541a240700d56c56c41377cddcadb4c618af86
vaishali_k <vaishali_k@techjoomla.com>
2017-06-02 17:08:16 +0530

    client and client id columns added

--- administrator/sql/install.mysql.utf8.sql
+++ administrator/sql/install.mysql.utf8.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS `#__hierarchy_users` (
 `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-
 `asset_id` INT(10) UNSIGNED NOT NULL DEFAULT '0',
-
-`user_id` INT(11)  NOT NULL ,
+`user_id` INT(11)  NOT NULL,
 `subuser_id` VARCHAR(255)  NOT NULL ,
+`client` VARCHAR(255)  NOT NULL ,
+`client_id` INT(11)  NOT NULL,
 PRIMARY KEY (`id`)
 ) DEFAULT COLLATE=utf8_general_ci;
 
