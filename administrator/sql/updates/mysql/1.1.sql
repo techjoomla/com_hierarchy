2fed42f4abaf3b869b414dc9e2565a6893c16746
Vaishali K <vaishali_k@techjoomla.com>
2018-01-09 19:23:57 +0530

    Fix: on install Hierarchy table does not exists

--- administrator/sql/updates/mysql/1.1.sql
+++ administrator/sql/updates/mysql/1.1.sql
@@ -1,3 +1,22 @@
+-- --------------------------------------------------------------
+--
+-- Table structure for table `#__hierarchy_users`
+--
+CREATE TABLE IF NOT EXISTS `#__hierarchy_users` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+`user_id` INT(11)  NOT NULL COMMENT 'This field is to store user id of user',
+`reports_to` INT(11)  NOT NULL COMMENT 'This field is to select to whom all current user reports to',
+`context` VARCHAR(255)  NOT NULL ,
+`context_id` INT(11)  NOT NULL,
+`created_by` INT(11)  NOT NULL,
+`modified_by` INT(11)  NOT NULL,
+`created_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+`modified_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+`state` INT(11)  NOT NULL,
+`note` TEXT NOT NULL,
+PRIMARY KEY (`id`)
+) DEFAULT COLLATE=utf8_general_ci;
+
 ALTER TABLE `#__hierarchy_users` CHANGE `user_id` `reports_to`  INT(11);
 ALTER TABLE `#__hierarchy_users` CHANGE `subuser_id` `user_id` INT(11);
 ALTER TABLE `#__hierarchy_users` CHANGE `client` `context` varchar(255);
