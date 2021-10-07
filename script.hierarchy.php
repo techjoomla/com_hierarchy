8616a5a5e5504bec0c0bb249478cd7bedb6b1ccb
kokbira <kokbira@gmail.com>
2021-10-07 09:08:33 -0300

    Little corrections on text (#50)

--- script.hierarchy.php
+++ script.hierarchy.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @version    SVN: <svn_id>
- * @package    Com_Tjlms
- * @copyright  Copyright (C) 2005 - 2014. All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- * Shika is free software. This version may have been modified pursuant
+ * @package    Com_Hierarchy
+ * @copyright  Copyright (C) 2016 - 2021 Techjoomla. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see http://www.gnu.org/licenses/gpl-2.0.html
+ * Hierarchy Management Extension is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
