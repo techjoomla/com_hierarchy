6d97c83194ad488ec3aef709c87f2fc1e047d8fa
Divya Chaudhari <divya_c@techjoomla.com>
2021-10-07 17:02:52 +0530

    Task #173952 Chore: Resolving Comments

--- administrator/sql/install.mysql.utf8.sql
+++ administrator/sql/install.mysql.utf8.sql
@@ -4,14 +4,14 @@
 --
 CREATE TABLE IF NOT EXISTS `#__hierarchy_users` (
 `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`user_id` INT(11)  NOT NULL COMMENT 'This field is to store user id of user',
-`reports_to` INT(11)  NOT NULL COMMENT 'This field is to select to whom all current user reports to',
+`user_id` INT(11)  NOT NULL COMMENT 'This field is to store user id of user' DEFAULT 0,
+`reports_to` INT(11)  NOT NULL COMMENT 'This field is to select to whom all current user reports to' DEFAULT 0,
 `context` VARCHAR(255)  NOT NULL DEFAULT '',
 `context_id` INT(11)  NOT NULL DEFAULT 0,
 `created_by` INT(11)  NOT NULL DEFAULT 0,
 `modified_by` INT(11)  NOT NULL DEFAULT 0,
-`created_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-`modified_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+`created_date` datetime NULL DEFAULT NULL,
+`modified_date` datetime NULL DEFAULT NULL,
 `state` INT(11)  NOT NULL DEFAULT 0,
 `note` TEXT NOT NULL DEFAULT '',
 PRIMARY KEY (`id`)
