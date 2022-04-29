7e5a06d2c5c31daba43b0734c04093f4480c7306
Ankush Maherwal <ankush_m@techjoomla.com>
2022-04-29 12:59:30 +0530

    Task #166370 feat: use namespace in overall product

--- administrator/sql/install.mysql.utf8.sql
+++ administrator/sql/install.mysql.utf8.sql
@@ -4,15 +4,15 @@
 --
 CREATE TABLE IF NOT EXISTS `#__hierarchy_users` (
 `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`user_id` INT(11)  NOT NULL COMMENT 'This field is to store user id of user' DEFAULT 0,
-`reports_to` INT(11)  NOT NULL COMMENT 'This field is to select to whom all current user reports to' DEFAULT 0,
-`context` VARCHAR(255)  NOT NULL DEFAULT '',
-`context_id` INT(11)  NOT NULL DEFAULT 0,
-`created_by` INT(11)  NOT NULL DEFAULT 0,
-`modified_by` INT(11)  NOT NULL DEFAULT 0,
+`user_id` INT(11) NOT NULL COMMENT 'This field is to store user id of user' DEFAULT 0,
+`reports_to` INT(11) NOT NULL COMMENT 'This field is to select to whom all current user reports to' DEFAULT 0,
+`context` VARCHAR(255) NOT NULL DEFAULT '',
+`context_id` INT(11) NOT NULL DEFAULT 0,
+`created_by` INT(11) NOT NULL DEFAULT 0,
+`modified_by` INT(11) NOT NULL DEFAULT 0,
 `created_date` datetime DEFAULT NULL,
 `modified_date` datetime DEFAULT NULL,
-`state` INT(11)  NOT NULL DEFAULT 0,
+`state` INT(11) NOT NULL DEFAULT 0,
 `note` TEXT DEFAULT NULL,
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
--- administrator/sql/updates/mysql/1.1.4.sql
+++ administrator/sql/updates/mysql/1.1.4.sql
@@ -6,5 +6,5 @@ ALTER TABLE `#__hierarchy_users` CHANGE `created_by` `created_by` INT(11)  NOT N
 ALTER TABLE `#__hierarchy_users` CHANGE `modified_by` `modified_by` INT(11)  NOT NULL DEFAULT 0;
 ALTER TABLE `#__hierarchy_users` CHANGE `created_date` `created_date` datetime DEFAULT NULL;
 ALTER TABLE `#__hierarchy_users` CHANGE `modified_date` `modified_date` datetime DEFAULT NULL;
-ALTER TABLE `#__hierarchy_users` CHANGE `state` `state` INT(11)  NOT NULL DEFAULT 0;
+ALTER TABLE `#__hierarchy_users` CHANGE `state` `state` INT(11) NOT NULL DEFAULT 0;
 ALTER TABLE `#__hierarchy_users` CHANGE `note` `note` TEXT DEFAULT NULL;
